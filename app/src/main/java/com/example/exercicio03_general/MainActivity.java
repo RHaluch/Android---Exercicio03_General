284f756285dfe779a817036f8a94970a0d6d655c
RHaluch <rodrigohaluch@hotmail.com>
2019-08-23 10:03:43 -0300

    Versão 2

--- app/src/main/java/com/example/exercicio03_general/MainActivity.java
+++ app/src/main/java/com/example/exercicio03_general/MainActivity.java
@@ -36,9 +36,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 5; i++) {
             valores.add(random.nextInt(6) + 1);
         }
-
         Collections.sort(valores);
-
         for (int i = 0; i < 5; i++) {
             jogadas += "(" + String.valueOf(i + 1) + ")Dado - Valor: " + String.valueOf(valores.get(i) + "\n");
         }
@@ -52,23 +50,29 @@ public class MainActivity extends AppCompatActivity {
 
         //jogada de 2
         if (Collections.frequency(valores, 2) > 0) {
-            if (pontos < Collections.frequency(valores, 2) * 2) {
-                pontos = Collections.frequency(valores, 2) * 2;
+            if(pontos==Collections.frequency(valores, 2) * 2) {
+                maiorPonto = maiorPonto + "Jogada de 2 = " + String.valueOf(Collections.frequency(valores, 2) * 2 + " Pontos\n");
+            }else if (pontos < Collections.frequency(valores, 2) * 2) {
                 maiorPonto = "Jogada de 2 = " + String.valueOf(Collections.frequency(valores, 2) * 2 + " Pontos\n");
+                pontos = Collections.frequency(valores, 2) * 2;
             }
         }
 
         //jogada de 3
         if (Collections.frequency(valores, 3) > 0) {
-            if (pontos < Collections.frequency(valores, 3) * 3) {
-                pontos = Collections.frequency(valores, 3) * 3;
+            if (pontos == Collections.frequency(valores, 3) * 3) {
+                maiorPonto = maiorPonto + "\nJogada de 3 = " + String.valueOf(Collections.frequency(valores, 3) * 3 + " Pontos\n");
+            }else if(pontos < Collections.frequency(valores, 3) * 3){
                 maiorPonto = "Jogada de 3 = " + String.valueOf(Collections.frequency(valores, 3) * 3 + " Pontos\n");
+                pontos = Collections.frequency(valores, 3) * 3;
             }
         }
 
         //jogada de 4
         if (Collections.frequency(valores, 4) > 0) {
-            if (pontos < Collections.frequency(valores, 4) * 4) {
+            if(pontos == Collections.frequency(valores, 4) * 4){
+                maiorPonto = maiorPonto + "Jogada de 4 = " + String.valueOf(Collections.frequency(valores, 4) * 4 + " Pontos\n");
+            }else if (pontos < Collections.frequency(valores, 4) * 4) {
                 pontos = Collections.frequency(valores, 4) * 4;
                 maiorPonto = "Jogada de 4 = " + String.valueOf(Collections.frequency(valores, 4) * 4 + " Pontos\n");
             }
@@ -76,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
         //jogada de 5
         if (Collections.frequency(valores, 5) > 0) {
-            if (pontos < Collections.frequency(valores, 5) * 5) {
+            if(pontos == Collections.frequency(valores, 5) * 5){
+                maiorPonto = maiorPonto + "Jogada de 5 = " + String.valueOf(Collections.frequency(valores, 5) * 5 + " Pontos\n");
+            }else if (pontos < Collections.frequency(valores, 5) * 5) {
                 pontos = Collections.frequency(valores, 5) * 5;
                 maiorPonto = "Jogada de 5 = " + String.valueOf(Collections.frequency(valores, 5) * 5 + " Pontos\n");
             }
@@ -84,7 +90,9 @@ public class MainActivity extends AppCompatActivity {
 
         //jogada de 6
         if (Collections.frequency(valores, 6) > 0) {
-            if (pontos < Collections.frequency(valores, 6) * 6) {
+            if(pontos == Collections.frequency(valores, 6) * 6){
+                maiorPonto = maiorPonto + "\nJogada de 6 = " + String.valueOf(Collections.frequency(valores, 6) * 6 + " Pontos\n");
+            }else if (pontos < Collections.frequency(valores, 6) * 6) {
                 pontos = Collections.frequency(valores, 6) * 6;
                 maiorPonto = "Jogada de 6 = " + String.valueOf(Collections.frequency(valores, 6) * 6 + " Pontos\n");
             }
@@ -94,11 +102,14 @@ public class MainActivity extends AppCompatActivity {
         if (Collections.frequency(valores, 1) == 3 || Collections.frequency(valores, 2) == 3 ||
                 Collections.frequency(valores, 3) == 3 || Collections.frequency(valores, 4) == 3 ||
                 Collections.frequency(valores, 5) == 3 || Collections.frequency(valores, 6) == 3) {
+
             int aux = 0;
             for (int i = 0; i < 5; i++) {
                 aux += Integer.parseInt(String.valueOf(valores.get(i)));
             }
-            if (pontos < aux) {
+            if(pontos==aux) {
+                maiorPonto = maiorPonto + "Trinca com " + pontos + " pontos\n";
+            }else if (pontos < aux) {
                 pontos = aux;
                 maiorPonto = "Trinca com " + pontos + " pontos\n";
             }
@@ -112,18 +123,23 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < 5; i++) {
                 aux += Integer.parseInt(String.valueOf(valores.get(i)));
             }
-            if (pontos < aux) {
+            if(pontos==aux) {
+                maiorPonto = maiorPonto + "Quadra com " + pontos + " pontos\n";
+            }else if (pontos < aux) {
                 pontos = aux;
                 maiorPonto = "Quadra com " + pontos + " pontos\n";
             }
         }
 
         //full-house
-        for (int i = 1; i < 7; i++) {
+        for (int i = 1; i < 6; i++) {
             if (Collections.frequency(valores, i) == 3) {
                 for (int j = i + 1; j <= 6; j++) {
                     if (Collections.frequency(valores, j) == 2) {
-                        if (pontos < 25) {
+                        if(pontos==25){
+                            maiorPonto = maiorPonto + "Full-House: " + pontos + " pontos\n";
+                            break;
+                        }else if (pontos < 25) {
                             pontos = 25;
                             maiorPonto = "Full-House: " + pontos + " pontos\n";
                             break;
@@ -132,11 +148,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        for (int i = 1; i < 7; i++) {
+        for (int i = 1; i < 6; i++) {
             if (Collections.frequency(valores, i) == 2) {
                 for (int j = i + 1; j <= 6; j++) {
                     if (Collections.frequency(valores, j) == 3) {
-                        if (pontos < 25) {
+                        if(pontos==25){
+                            maiorPonto = maiorPonto + "Full-House: " + pontos + " pontos\n";
+                            break;
+                        }else if (pontos < 25) {
                             pontos = 25;
                             maiorPonto = "Full-House: " + pontos + " pontos\n";
                             break;
@@ -154,7 +173,9 @@ public class MainActivity extends AppCompatActivity {
         aux.add(5);
         aux.add(6);
         if(valores.equals(aux)){
-            if (pontos < 30) {
+            if(pontos==30){
+                maiorPonto = maiorPonto + "Sequencia Alta: " + pontos + " pontos\n";
+            }else if (pontos < 30) {
                 pontos = 30;
                 maiorPonto = "Sequencia Alta: " + pontos + " pontos\n";
             }
@@ -168,7 +189,9 @@ public class MainActivity extends AppCompatActivity {
         aux.add(4);
         aux.add(5);
         if(valores.equals(aux)){
-            if (pontos < 40) {
+            if(pontos==40){
+                maiorPonto = maiorPonto + "Sequencia Alta: " + pontos + " pontos\n";
+            }else if (pontos < 40) {
                 pontos = 40;
                 maiorPonto = "Sequencia Baixa: " + pontos + " pontos\n";
             }
@@ -179,7 +202,9 @@ public class MainActivity extends AppCompatActivity {
                 Collections.frequency(valores,3)==5 || Collections.frequency(valores,4)==5 ||
                 Collections.frequency(valores,5)==5 || Collections.frequency(valores,6)==5){
 
-            if (pontos < 50) {
+            if(pontos==50){
+                maiorPonto = maiorPonto + "\nYam (General): " + pontos + " pontos\n";
+            }else if (pontos < 50) {
                 pontos = 50;
                 maiorPonto = "Yam (General): " + pontos + " pontos\n";
             }
@@ -190,10 +215,12 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<5;i++){
             soma += Integer.parseInt(String.valueOf(valores.get(i)));
         }
-        if (pontos < soma) {
+        if(pontos==soma){
+            maiorPonto = maiorPonto + "Jogada Aleatoria: " + pontos + " pontos\n";
+        }else if (pontos < soma) {
             pontos = soma;
             maiorPonto = "Jogada Aleatoria: " + pontos + " pontos\n";
         }
-        resultado.setText(jogadas + "\n" + "Maior classificação: " + maiorPonto);
+        resultado.setText(jogadas + "\n" + "Maior classificação(ções)\n\n" + maiorPonto);
     }
 }
